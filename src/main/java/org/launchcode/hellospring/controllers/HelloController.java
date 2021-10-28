8ffac82a73ac74fc05055cd9762da7194b023b85
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-10-28 15:48:46 -0400

    successfully added a path for tha handler

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-    @GetMapping
+    // handles request at path /hello
+    // some annotations can accept some parameters and parenthesis
+    @GetMapping("hello") // the path we want the handler to live at
     @ResponseBody
     public String hello() {
         return "Hello, Spring!";
