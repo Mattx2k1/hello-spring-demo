762497c77d13fc6e97efa2541a2ddbc7892f248c
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-10-28 16:12:11 -0400

    added dynamic request handler using query params

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,6 +2,7 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,16 +10,27 @@ public class HelloController {
 
     // handles request at path /hello
     // some annotations can accept some parameters and parenthesis
-    @GetMapping("hello") // the path we want the handler to live at
-    @ResponseBody
-    public String hello() {
-        return "Hello, Spring!";
-    }
+    // When two handlers live at the same address?:
+    // Caused by: java.lang.IllegalStateException: Ambiguous mapping. Cannot map 'helloController' method
+
+//    @GetMapping("hello") // the path we want the handler to live at
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
 
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye() {
         return "Goodbye, Springy!";
+    }
+
+    // Handler that handles requests of the form /hello?name=LaunchCode
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+
     }
 
 }
